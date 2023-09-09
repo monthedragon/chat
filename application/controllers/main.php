3a163b7b178f0cd32dcee687eec7f3b9b620b1e7
monthedragon <mon_tuyay22@yahoo.com>
2023-09-10 00:02:19 +0800

    Customized chat log, added search

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,1 +1,41 @@
-<?php $path = '/var/www/html/template/application/third_party/php/pear';set_include_path(get_include_path() . PATH_SEPARATOR . $path);$_SERVER['DOCUMENT_ROOT'] .= '/template';include_once($_SERVER['DOCUMENT_ROOT']. '/application/third_party/Spreadsheet/Excel/Writer.php');include_once($_SERVER['DOCUMENT_ROOT']. '/application/third_party/Spreadsheet/Excel/Reader/reader.php');//include_once('Spreadsheet/Excel/Writer.php');//include_once('Spreadsheet/Excel/Reader/reader.php');Class Main extends Auth_Controller  { 	var $data = array();	//use `Auth_Controller` if you want the page to be validated if the user is logged in or not, if you want to disable this then use `CI_Controller` instead	public  function __construct(){		parent::__construct();		$this->load->model('main_model');		$this->load->helper('url');		$this->load->library('session');  		$this->load->helper(array('form', 'url'));  		#$this->has_permission(178);		$this->data = $this->get_privs();		/**load your own library			or add in application/config/autoload/ under libraries		**/		//$this->load->library('MY_auth_lib');	} 		public function index(){		$this->load->helper('form');				$this->set_header_data(PROJECT_NAME);					$dataIndex['session'] = $this->session->all_userdata();		$dataIndex['privs'] = $this->data;		$this->load->view('main/index',$dataIndex);				$this->load->view('templates/footer'); 		}	}?>
+<?php 
+$path = BASEPATH .'../application/third_party/php/pear';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+$_SERVER['DOCUMENT_ROOT'] .= '/template';
+
+include_once($_SERVER['DOCUMENT_ROOT']. '/application/third_party/Spreadsheet/Excel/Writer.php');
+include_once($_SERVER['DOCUMENT_ROOT']. '/application/third_party/Spreadsheet/Excel/Reader/reader.php');
+//include_once('Spreadsheet/Excel/Writer.php');
+//include_once('Spreadsheet/Excel/Reader/reader.php');
+Class Main extends Auth_Controller  { 
+	var $data = array();
+	//use `Auth_Controller` if you want the page to be validated if the user is logged in or not, if you want to disable this then use `CI_Controller` instead
+	public  function __construct(){
+		parent::__construct();
+		$this->load->model('main_model');
+		$this->load->helper('url');
+		$this->load->library('session');  
+		$this->load->helper(array('form', 'url'));  
+		#$this->has_permission(178);
+		$this->data = $this->get_privs();
+		/**load your own library
+			or add in application/config/autoload/ under libraries
+		**/
+		//$this->load->library('MY_auth_lib');
+	} 
+	
+	public function index(){
+		$this->load->helper('form');
+		
+		$this->set_header_data(PROJECT_NAME);
+			
+		$dataIndex['session'] = $this->session->all_userdata();
+		$dataIndex['privs'] = $this->data;
+		$this->load->view('main/index',$dataIndex);
+		
+		$this->load->view('templates/footer'); 	
+	}
+	
+}
+?>
--- application/views/chat/user_list.php
+++ application/views/chat/user_list.php
@@ -1,141 +1,164 @@
 <?php
-	
-echo "<form id = 'frm-user-list'>";	
 
-if($user_type == ADMIN_CODE){
+echo "<form id = 'frm-user-list'>";
+
+if ($user_type == ADMIN_CODE) {
 	echo "<input type='input' class=required name='gc_name' style='width:80%;margin-bottom:10px;'>";
 	echo "<input type=submit value='Create Group chat' id='btn-crate-gc' style='width:83%' class='button'><br>";
 	echo '<hr>';
-	
+	echo "<input type='input' id='user_chat_search' name='user_chat_search' style='width:80%;margin-bottom:10px;' placeholder='search name here'>";
 }
-	echo "<div style='max-height:350px;overflow-x:hidden;overflow-y:auto;'>";
-	foreach($users as $details){
-		$unread_cls = '';
-		if(isset($unread_user_chat[$details['user_name']])){
-			$unread_cls = 'div-unread';
-		}
-		
-		$checkbox = ($user_type == ADMIN_CODE) ? "<input type=checkbox value='{$details['user_name']}' class='cursor-pointer' name='participants[]'>" : '';
-		echo "{$checkbox}<span style='display:inline-block;height:15px;width:80%;' class='div-user-chat cursor-pointer {$unread_cls}' user_id='{$details['user_name']}' >  {$details['firstname']}  {$details['lastname']}</span>";
-		echo '<br>';
+echo "<div style='height:350px;max-height:350px;overflow-x:hidden;overflow-y:auto;'>";
+foreach ($users as $details) {
+	$unread_cls = '';
+	if (isset($unread_user_chat[$details['user_name']])) {
+		$unread_cls = 'div-unread';
 	}
-	echo '</div>';
+
+	$fullName = "{$details['firstname']}  {$details['lastname']}";
+	$fullNameCls = strtolower(str_replace(' ','',$fullName));
+	$checkbox = ($user_type == ADMIN_CODE) ? "<input type=checkbox value='{$details['user_name']}' class='cursor-pointer' name='participants[]'>" : '';
+	echo "<div class='div-user {$fullNameCls}' style='margin:1px;display:flex; text-align:left align-items: center;'>";
+	echo "{$checkbox}<span style='display:inline-block;height:15px;width:80%;margin-top:5px;' class='div-user-chat cursor-pointer {$unread_cls}' user_id='{$details['user_name']}' >{$fullName}</span>";
+	echo "</div>";
+	// echo '<br>';
+}
+echo '</div>';
 echo '</form>';
 
-	if(isset($gc_list)){
-		echo '<hr>';
-		//2023-02-26 (CHAT GC)
-		foreach($gc_list as $details){
-			$unread_cls = '';
-			if(isset($unread_group_chat[$details['chat_id']])){
-				$unread_cls = 'div-unread';
-			}
-			echo "<div style='width:80%;float:left;' class='div-gc-chat cursor-pointer {$unread_cls}' chat_id='{$details['chat_id']}'>{$details['chat_name']}</div>";
-			
-			if($user_type == ADMIN_CODE){
-				echo "<div style='width:15%;float:right;' class='cursor-pointer e-link'  chat_id='{$details['chat_id']}'>edit</div>";
-			}
-			
-		
+if (isset($gc_list)) {
+	echo '<hr>';
+	//2023-02-26 (CHAT GC)
+	foreach ($gc_list as $details) {
+		$unread_cls = '';
+		if (isset($unread_group_chat[$details['chat_id']])) {
+			$unread_cls = 'div-unread';
+		}
+		echo "<div style='width:80%;float:left;' class='div-gc-chat cursor-pointer {$unread_cls}' chat_id='{$details['chat_id']}'>{$details['chat_name']}</div>";
+
+		if ($user_type == ADMIN_CODE) {
+			echo "<div style='width:15%;float:right;' class='cursor-pointer e-link'  chat_id='{$details['chat_id']}'>edit</div>";
 		}
 	}
+}
 ?>
 
 <script>
-var default_title = '<?=PROJECT_NAME?>';
-var position = 0;
+	var default_title = '<?= PROJECT_NAME ?>';
+	var position = 0;
 
-//Make the title scrolling to notice easily the new messages
-function scrolltitle() {
-	var unreadCtr = $('.div-unread').length;
-	if(unreadCtr > 0){
-		if(unreadCtr == 1){
-			msg = 'You have a message from ' + $('.div-unread').html() + ' . . . ';
-		}else{
-			msg = 'You have '+unreadCtr+' messages . . .';
+	//Make the title scrolling to notice easily the new messages
+	function scrolltitle() {
+		var unreadCtr = $('.div-unread').length;
+		if (unreadCtr > 0) {
+			if (unreadCtr == 1) {
+				msg = 'You have a message from ' + $('.div-unread').html() + ' . . . ';
+			} else {
+				msg = 'You have ' + unreadCtr + ' messages . . .';
+			}
+
+			document.title = msg.substring(position, msg.length) + msg.substring(0, position);
+			position++;
+			if (position > msg.length) position = 0
+			window.setTimeout("scrolltitle()", 200);
 		}
-		
-		document.title = msg.substring(position, msg.length) + msg.substring(0, position); 
-		position++;
-		if (position > msg.length) position = 0
-		window.setTimeout("scrolltitle()",200);
 	}
-}
 
 
-	$(function(){
-		$(".div-user-chat, .div-gc-chat").mouseover(function(){$(this).addClass('highlight_cls');})
-		$(".div-user-chat, .div-gc-chat").mouseout(function(){$(this).removeClass('highlight_cls');})
-		
-		$('.e-link').mouseover(function(){$(this).css('font-weight','bold')});
-		$('.e-link').mouseout(function(){$(this).css('font-weight','normal')});
-		
-		$('#frm-user-list').submit(function(event){
-			
+	$(function() {
+		$(".div-user-chat, .div-gc-chat").mouseover(function() {
+			$(this).addClass('highlight_cls');
+		})
+		$(".div-user-chat, .div-gc-chat").mouseout(function() {
+			$(this).removeClass('highlight_cls');
+		})
+
+		$('.e-link').mouseover(function() {
+			$(this).css('font-weight', 'bold')
+		});
+		$('.e-link').mouseout(function() {
+			$(this).css('font-weight', 'normal')
+		});
+
+		$('#frm-user-list').submit(function(event) {
+
 			event.preventDefault();
-			if($(this).valid()){
+			if ($(this).valid()) {
 				$.ajax({
-					url:'<?=base_url()?>chat/createGC',
-					data:$(this).serialize(),
-					type:'POST',
+					url: '<?= base_url() ?>chat/createGC',
+					data: $(this).serialize(),
+					type: 'POST',
 					// beforeSend:function(){$("#btnSubmit").val('please wait...').prop('disabled',true)},
-					success:function(data){
-						
+					success: function(data) {
+
 						// console.log(data);
-						if(data != ''){
+						if (data != '') {
 							alert(data);
-						}else{
+						} else {
 							alert('Saved');
 							location.reload(true);
 						}
-					
+
 					}
 				});
 			}
-			
+
 		})
-		
-		if($('.div-unread').length > 0){
+
+		if ($('.div-unread').length > 0) {
 			scrolltitle();
-		}else{
+		} else {
 			document.title = default_title;
 		}
-		
+
 		$('.div-user-chat').unbind('click');
-		$('.div-user-chat').click(function(){
+		$('.div-user-chat').click(function() {
 			var user_id = $(this).attr('user_id');
-			var url  ='<?=base_url()?>chat/chat_box/'+user_id;
+			var url = '<?= base_url() ?>chat/chat_box/' + user_id;
 			var target_interval = $('#interval_holder').html(); //get the targeted intervalId to be cleared once new chat intialized
-			
-			do_ajax(url,'POST','','div-chat-log');
-			
+
+			do_ajax(url, 'POST', '', 'div-chat-log');
+
 			clearInterval(target_interval); //clear previously created interval to avoid unexpected request
 		})
-		
+
 		//start of GC
 		$('.div-gc-chat').unbind('click');
-		$('.div-gc-chat').click(function(){
+		$('.div-gc-chat').click(function() {
 			var chat_id = $(this).attr('chat_id');
-			var url  ='<?=base_url()?>chat/gc_chat_box/'+chat_id;
+			var url = '<?= base_url() ?>chat/gc_chat_box/' + chat_id;
 			var target_interval = $('#interval_holder').html(); //get the targeted intervalId to be cleared once new chat intialized
-			
-			do_ajax(url,'POST','','div-chat-log');
-			
+
+			do_ajax(url, 'POST', '', 'div-chat-log');
+
 			clearInterval(target_interval); //clear previously created interval to avoid unexpected request
 		})
-		
+
 		//start of edit GC 
-		
+
 		$('.e-link').unbind('click');
-		$('.e-link').click(function(){
+		$('.e-link').click(function() {
 			var chat_id = $(this).attr('chat_id');
-			var url  ='<?=base_url()?>chat/edit_gc/'+chat_id;
+			var url = '<?= base_url() ?>chat/edit_gc/' + chat_id;
 			var target_interval = $('#interval_holder').html(); //get the targeted intervalId to be cleared once new chat intialized
-			
-			do_ajax(url,'POST','','div-chat-log');
-			
+
+			do_ajax(url, 'POST', '', 'div-chat-log');
+
 			clearInterval(target_interval); //clear previously created interval to avoid unexpected request
 		})
-		
+
+		// Listen for input changes in the wildcard input field
+		$('#user_chat_search').on('input', function() {
+			var wildclass = $(this).val().toLowerCase();
+
+			// Hide all div elements initially
+			$('.div-user').hide();
+
+			// Show div elements whose class attribute matches the wildcard pattern
+			$('.div-user').filter(function() {
+				return new RegExp(wildclass).test($(this).attr('class'));
+			}).show();
+		});
+
 	})
 </script>
